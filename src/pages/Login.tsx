30c585df836ea97a97d81e523624c1ddcaba0abc
[dyad] <automatizavps@gmail.com>
2025-10-23 04:34:26 -0300

    [dyad] Configurando o Supabase Auth para usar localização em Português do Brasil - wrote 1 file(s)

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -26,6 +26,48 @@ const Login = () => {
             theme="light"
             view="sign_in"
             redirectTo={window.location.origin + '/'}
+            // Adiciona a localização para Português do Brasil
+            localization={{
+              variables: {
+                sign_in: {
+                  email_label: 'Seu email',
+                  password_label: 'Sua senha',
+                  email_input_placeholder: 'Digite seu email',
+                  password_input_placeholder: 'Digite sua senha',
+                  button_label: 'Entrar',
+                  loading_button_label: 'Entrando...',
+                  link_text: 'Já tem uma conta? Faça login',
+                },
+                sign_up: {
+                  email_label: 'Seu email',
+                  password_label: 'Crie uma senha',
+                  email_input_placeholder: 'Digite seu email',
+                  password_input_placeholder: 'Crie uma senha',
+                  button_label: 'Cadastrar',
+                  loading_button_label: 'Cadastrando...',
+                  link_text: 'Não tem uma conta? Cadastre-se',
+                },
+                forgotten_password: {
+                  email_label: 'Seu email',
+                  email_input_placeholder: 'Digite seu email para recuperação',
+                  button_label: 'Enviar instruções de recuperação',
+                  loading_button_label: 'Enviando...',
+                  link_text: 'Esqueceu sua senha?',
+                },
+                update_password: {
+                  password_label: 'Nova senha',
+                  password_input_placeholder: 'Digite sua nova senha',
+                  button_label: 'Atualizar senha',
+                  loading_button_label: 'Atualizando...',
+                },
+                magic_link: {
+                  email_input_placeholder: 'Digite seu email',
+                  button_label: 'Enviar link mágico',
+                  loading_button_label: 'Enviando link mágico...',
+                  link_text: 'Enviar um link mágico por email',
+                },
+              },
+            }}
           />
         </div>
       </div>
