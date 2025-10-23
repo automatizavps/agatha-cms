480878c065712fbc71279343adb7f45130cc24fb
[dyad] <automatizavps@gmail.com>
2025-10-23 16:43:18 -0300

    Reverted all changes back to version abd379537e367425381ec4759c215d829a4c6dbd

--- src/components/DailyServiceByHourChart.tsx
+++ src/components/DailyServiceByHourChart.tsx
@@ -72,7 +72,7 @@ export default function DailyServiceByHourChart({ companyId }: DailyServiceByHou
             margin={{
               top: 5,
               right: 5,
-              left: 0, // Alterado de -15 para 0
+              left: -15,
               bottom: 0,
             }}
           >
