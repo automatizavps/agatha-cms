5c716024c3ef5681cadf3b71fb8a1ffeefafe1d5
[dyad] <automatizavps@gmail.com>
2025-10-27 16:27:37 -0300

    [dyad] Importing `useFieldArray` in PromotionForm.tsx - wrote 1 file(s)

--- src/components/PromotionForm.tsx
+++ src/components/PromotionForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { useForm, useFieldArray } from "react-hook-form"; // ADDED useFieldArray
 import * as z from "zod";
 import { Button } from "@/components/ui/button";
 import {
